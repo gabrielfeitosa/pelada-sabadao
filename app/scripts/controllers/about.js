ac09e385a10020b14e6598907aff03d532fe0f3f
Gabriel Feitosa <gabfeitosa@gmail.com>
2015-11-05 00:24:09 -0200

    init pelada

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name peladaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the peladaApp
+ */
+angular.module('peladaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name peladaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the peladaApp
+ */
+angular.module('peladaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
